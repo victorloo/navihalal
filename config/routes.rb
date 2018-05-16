701eafdfc4c72f38494c295a6692dfac58dadfbc
Tony Lin <tonylin1208@gmail.com>
2018-05-17 03:11:18 +0800

    add admin user controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root 'restaurants#index'
   resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root 'users#index'
+    resources :restaurants
+    resources :users
+  end
 end
