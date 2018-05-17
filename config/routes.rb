cbbf3eb9415dd98fd93869cc4a9cdde02ecd755a
Tony Lin <tonylin1208@gmail.com>
2018-05-17 20:28:31 +0800

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'restaurants#index'
   resources :restaurants
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     root 'users#index'
