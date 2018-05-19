04ffbb935bc875336cb6e3470d10f9f402bac385
Tony Lin <tonylin1208@gmail.com>
2018-05-19 12:21:26 +0800

    add Collect

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_19_041407) do
+ActiveRecord::Schema.define(version: 2018_05_19_042025) do
+
+  create_table "collects", force: :cascade do |t|
+    t.integer "restaurant_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
