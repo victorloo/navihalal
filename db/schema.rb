f5efcfa41ce8688dc613d40a76f102b556094e2e
Tony Lin <tonylin1208@gmail.com>
2018-05-19 12:17:43 +0800

    add Friendship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_18_044217) do
+ActiveRecord::Schema.define(version: 2018_05_19_041407) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "gmaps", force: :cascade do |t|
     t.float "latitude"
