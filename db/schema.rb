dc40b7fd6cf858df6fe528533ecf93eeae406400
victorloo <ila9970@gmail.com>
2018-05-21 00:40:49 +0800

    Updated schema: remove datatable "gmaps"

--- db/schema.rb
+++ db/schema.rb
@@ -27,14 +27,6 @@ ActiveRecord::Schema.define(version: 2018_05_19_042025) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "gmaps", force: :cascade do |t|
-    t.float "latitude"
-    t.float "longitude"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "intro"
